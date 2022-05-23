18d4e143d3be829e12c95503dfae3fc619aea81c
bottarocarlo <bottarocarloo@gmail.com>
2022-05-23 12:01:16 +0200

    Created component Mensa

---
+++ components/Mensa/index.js
@@ -0,0 +1,16 @@
+const {useState , useEffect} = require("react");
+const Image = require("next/image");
+const axios = require("axios");
+
+
+module.exports = function Mensa(props) {
+  return (
+    <div className="absolute top-40 left-72 flex flex-col gap">
+      <h1 className=" font-semibold text-lg py-3">Menù del giorno</h1>
+      <div className=" border border-gray-200 shadow-md text-gray-700   rounded bg-white p-3 ">
+        <h2 className="font-medium py-2">primo: </h2>
+        <h2 className="font-medium py-2">secondo: </h2>
+      </div>
+    </div>
+  );
+}
