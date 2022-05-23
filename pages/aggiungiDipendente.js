3add5a6c7a717897b9a13a826335c549764b6391
Gio200023 <lunardigiovanni@icloud.com>
2022-05-23 11:45:03 +0200

    addedd cloud aziendale page

--- pages/aggiungiDipendente.js
+++ pages/aggiungiDipendente.js
@@ -5,8 +5,41 @@ const TopBar = require("../components/topBar");
 module.exports =  function aggiungiDipendente() {
 
       return (
-        <div>
+                <div>
           <SidebarAA />
-          </div>
-           );
+          <TopBar />
+          <form onSubmit={handleAdd}>
+            <div className="  flex flex-col gap-5 justify-center items-center">
+              <input
+                className="border  rounded p-2"
+                type="text"
+                id="usernameInput"
+                placeholder="Username Dipendente"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              
+              <input
+                className="border  rounded p-2"
+                type="text"
+                id="DipX"
+                placeholder="Livello Dipendete"
+                onChange={(e) => setRole(e.target.value)}
+              />
+             
+              <button
+                type="submit"
+                className="relative  py-2 inline-block px-6 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md  hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                Aggiungi
+              </button>
+            </div>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>ERROR!!</h1>
+        </div>
+      );
 }
--- pages/cloudAziendale.js
+++ pages/cloudAziendale.js
@@ -1,12 +1,73 @@
-const SidebarAA = require("../components/sidebarAA");
-const SidebarDIP = require("../components/sidebarAA");
-const TopBar = require("../components/topBar");
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import home from '../public/home-outline.png'
+import React, { Component, useEffect, useState } from 'react';
+import {MdFirstPage, MdOutlineLogin , MdBeachAccess , MdLocalHospital , MdContacts , MdCloud} from 'react-icons/md';
+import { SidebarDip } from '../components/sidebarDip'
+import { SidebarAA } from '../components/sidebarAA'
+import TopBar from '../components/topBar'
 
-module.exports =  function aggiungiDipendente() {
+function parseJwt(token) {
+  if (!token) { return; }
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace('-', '+').replace('_', '/');
+  return JSON.parse(window.atob(base64));
+}
 
+export default function cloudAziedale() {
+  const [jwt, setJwt] = useState("");
+  const [rt, setRt] = useState("");
+  const [set, setSet] = useState(1);
+
+  //const [jwt, setJwt] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setJwt(sessionStorage.getItem('jwt'));
+      setRt(sessionStorage.getItem('rt'));
+      console.log("JWT-->", jwt);
+      setSet(0);
+    }, 50);
+  }, []);
+
+  if (set) {
+    return (
+      <div>
+      </div>
+    );
+  }
+
+  else {
+    console.log("JWT-->", jwt);
+
+    if (jwt == undefined) {
       return (
         <div>
-          <SidebarDIP/>
-          </div>
-           );
+          <h1>Devi prima effettuare il login!</h1>
+          <a href='/'>Vai alla pagina di login</a>
+        </div>
+      );
+    }
+
+    else if (parseJwt(jwt).role == "DIP0" || parseJwt(jwt).role == "DIP1") {
+      //delete sessionStorage.jwt;
+      return (
+        <div>
+          <SidebarDip/>
+          <TopBar/>
+        </div>
+      );
+    }
+
+    else {
+      return (
+        <div>
+          <h1>ERROR!!</h1>
+        </div>
+      );
+    }
+  }
+
 }
